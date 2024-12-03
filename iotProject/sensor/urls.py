00120f3443084a37e9a1da17567cddb43333687e
Hepheir <hepheir@gmail.com>
2024-12-04 05:16:13 +0900

    chore(sensor.urls): add `sensor/radar/`, `seat/` endpoints

--- iotProject/sensor/urls.py
+++ iotProject/sensor/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('sensor/radar/', views.RadarSensorListCreate.as_view()),
+    path('seat/', views.SeatList.as_view()),
 ]
