a836b06c655ee1b4e02a8526f2eb7b8e92ac1a28
Hepheir <hepheir@gmail.com>
2024-12-04 04:41:24 +0900

    feat(sensor.serializers): create `RadarSensorSerializer`

--- iotProject/sensor/serializers.py
+++ iotProject/sensor/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from . import models
+
+
+class RadarSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.RadarSensor
+        fields = '__all__'
