d41b06145888b6ac5a1a0925fb7dd03d026ff318
Hepheir <hepheir@gmail.com>
2024-12-04 05:14:00 +0900

    chore(sensor.views): add Radar, Seat RestAPI

--- iotProject/sensor/views.py
+++ iotProject/sensor/views.py
@@ -2,3 +2,13 @@ from rest_framework import generics
 
 from . import models
 from . import serializers
+
+
+class RadarSensorListCreate(generics.ListCreateAPIView):
+    queryset = models.RadarSensor
+    serializer_class = serializers.RadarSensorSerializer
+
+
+class SeatList(generics.ListAPIView):
+    queryset = models.Seat
+    serializer_class = serializers.SeatSerializer
