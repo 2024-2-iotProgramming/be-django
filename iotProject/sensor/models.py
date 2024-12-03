4302cbf37a6994222380d96480c35fe51b44c9b8
Hepheir <hepheir@gmail.com>
2024-12-04 07:39:11 +0900

    refactor(sensor.models): 좌석 표시에 x, y 좌표계 사용

--- iotProject/sensor/models.py
+++ iotProject/sensor/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 
 
-class RadarSensorDirection(models.TextChoices):
-	Y_POS = '+y'
-	Y_NEG = '-y'
-
-
 class RadarSensor(models.Model):
-	model = models.CharField(default='HC-SR04', max_length=20)
-	position = models.IntegerField()
-	direction = models.TextField(choices=RadarSensorDirection.choices)
-	echo_cm = models.FloatField()
-	timestamp = models.DateTimeField()
+    x = models.IntegerField(choices=[(0, 0), (1, 1)])
+    y = models.IntegerField()
+    model = models.CharField(default='HC-SR04', max_length=20)
+    echo_cm = models.FloatField()
+    timestamp = models.DateTimeField()
 
 
 class Seat(models.Model):
-	seat_number = models.IntegerField()
-	occupied = models.BooleanField(default=False)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    occupied = models.BooleanField(default=False)
