3afb6815c03dc6f48b2240e50058a4449fc282cb
Hepheir <hepheir@gmail.com>
2024-12-04 07:03:04 +0900

    refactor: 모델 필드 추가 (RadarSensor.position)

--- iotProject/sensor/models.py
+++ iotProject/sensor/models.py
@@ -8,6 +8,7 @@ class RadarSensorDirection(models.TextChoices):
 
 class RadarSensor(models.Model):
 	model = models.CharField(default='HC-SR04', max_length=20)
+	position = models.IntegerField()
 	direction = models.TimeField(choices=RadarSensorDirection.choices)
 	echo_cm = models.FloatField()
 	timestamp = models.DateTimeField()
