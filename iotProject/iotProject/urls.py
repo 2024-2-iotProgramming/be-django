06996e118098c7bbe715a3e013960165b3ccbf5f
Hepheir <hepheir@gmail.com>
2024-12-04 06:49:43 +0900

    fix: wrong url endpoint, remove unused libraries

--- iotProject/iotProject/urls.py
+++ iotProject/iotProject/urls.py
@@ -34,7 +34,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('sensor/', include('sensor.urls')),
+    path('', include('sensor.urls')),
     path('admin/', admin.site.urls),
     path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
