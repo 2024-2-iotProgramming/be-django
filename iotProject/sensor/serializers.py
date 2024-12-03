5c7eb41fa7ab704e82caba184a2d793ea5ac211b
Hepheir <hepheir@gmail.com>
2024-12-04 04:41:54 +0900

    feat(sensor.serializers): create `SeatSerializer`

--- iotProject/sensor/serializers.py
+++ iotProject/sensor/serializers.py
@@ -7,3 +7,9 @@ class RadarSensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.RadarSensor
         fields = '__all__'
+
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Seat
+        fields = '__all__'
