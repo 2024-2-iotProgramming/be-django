2fcac028b8a9da04a993c058aaa635f9eb161dca
Hepheir <hepheir@gmail.com>
2024-12-04 07:19:09 +0900

    refactor(sensor.views): serializers.py 의 내용 병합 및 쓰로틀링 추가 (20 req / sec)

--- iotProject/sensor/views.py
+++ iotProject/sensor/views.py
@@ -1,14 +1,36 @@
 from rest_framework import generics
+from rest_framework import serializers
+from rest_framework import throttling
 
 from . import models
-from . import serializers
+
+
+class RadarSensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.RadarSensor
+        fields = '__all__'
+
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Seat
+        fields = '__all__'
+
+
+class RadorSensorCreateThrottle(throttling.SimpleRateThrottle):
+    scope = 'radar_sensor_create'
+    rate = '20/sec'
+
+    def get_cache_key(self, request, view):
+        return True
 
 
 class RadarSensorListCreate(generics.ListCreateAPIView):
     queryset = models.RadarSensor
-    serializer_class = serializers.RadarSensorSerializer
+    serializer_class = RadarSensorSerializer
+    throttle_classes = [RadorSensorCreateThrottle]
 
 
 class SeatList(generics.ListAPIView):
     queryset = models.Seat
-    serializer_class = serializers.SeatSerializer
+    serializer_class = SeatSerializer
